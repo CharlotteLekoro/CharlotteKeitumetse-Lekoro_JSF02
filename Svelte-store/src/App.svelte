8d32ac02f3a5d6972dc64f6f28e6520f9a7c0418
CharlotteLekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-07-31 04:42:18 +0200

    navbar added and styled

--- Svelte-store/src/App.svelte
+++ Svelte-store/src/App.svelte
@@ -1,47 +1,60 @@
-<script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-</script>
-
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+// Navbar.svelte
+<nav class="navbar">
+  <div class="logo">
+    <img src="logo.png" alt="Trendy Treasures Logo" />
+    <span>Trendy Treasures</span>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+  <ul class="nav-links">
+    <li>
+      <a href="#" class="nav-link">Add to Cart</a>
+    </li>
+    <li>
+      <a href="#" class="nav-link">Wishlist</a>
+    </li>
+  </ul>
+</nav>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+ .navbar {
+    position: fixed; /* fix the navbar to the top of the page */
+    top: 0;
+    left: 0;
+    width: 100%; /* stretch across the whole page */
+    height: 60px; /* adjust the height to your liking */
+    background-color: #653b73;
+    color: #fff;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px; /* add some padding for the logo and links */
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+ .logo {
+    display: flex;
+    align-items: center;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+
+ .logo img {
+    width: 30px;
+    height: 30px;
+    margin-right: 10px;
   }
-  .read-the-docs {
-    color: #888;
+
+ .nav-links {
+    display: flex;
+    list-style: none;
+    margin: 20px;
+    padding: 0;
+  }
+
+ .nav-link {
+    color: #fff;
+    text-decoration: none;
+    margin-right: 20px;
+    margin-left: 20px;
+  }
+
+ .nav-link:hover {
+    color: #25a327;
   }
 </style>
